b600570458cb6190c74b66feaf3fd923cdd83627
Zlatimir Petrov <107613362+petroff69@users.noreply.github.com>
2024-10-14 00:41:11 +0300

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "sonner";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-	title: "2txt",
+	title: "Sentient Image Description Collector Prototype",
 	description: "Image to text, fast.",
 };
 
@@ -21,7 +21,7 @@ export default function RootLayout({
 				className={`${GeistSans.variable} ${GeistMono.variable} font-mono bg-gray-50 dark:bg-gray-950 text-black dark:text-white px-3 lg:px-10 py-4 lg:py-10 min-h-dvh flex flex-col`}
 			>
 				<h1 className="font-semibold text-center text-2xl bg-gradient-to-b dark:from-gray-50 dark:to-gray-200 from-gray-950 to-gray-800 bg-clip-text text-transparent select-none">
-					2txt
+				Sentient Image Description Collector Prototype
 				</h1>
 
 				<main className="grow flex flex-col lg:flex-row gap-6 py-4 lg:py-10">
@@ -30,8 +30,8 @@ export default function RootLayout({
 
 				<footer className="lg:flex flex-row justify-between text-center text-sm dark:text-gray-400 text-gray-600 select-none">
 					<p>
-						<A href="https://github.com/ai-ng">ai-ng</A> /{" "}
-						<A href="https://nickoates.com">nick oates</A>
+						<A href="https://github.com/petroff69">sentient-ai</A> /{" "}
+						<A href="https://zlatimir-petroff-portfolio.vercel.app/">zlatimir petrov</A>
 					</p>
 					<p>
 						Built with{" "}
@@ -39,10 +39,7 @@ export default function RootLayout({
 						<A href="https://claude.ai/">Claude</A>
 					</p>
 					<p>
-						<A href="https://github.com/ai-ng/2txt">source</A> /{" "}
-						<A href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fai-ng%2F2txt&env=ANTHROPIC_API_KEY&envDescription=Your%20Anthropic%20API%20key%20from%20https%3A%2F%2Fconsole.anthropic.com&envLink=https%3A%2F%2Fconsole.anthropic.com%2F&project-name=2txt&repository-name=2txt&demo-title=2txt&demo-description=Image%20to%20text%2C%20fast.&demo-url=https%3A%2F%2F2txt.vercel.app&demo-image=https%3A%2F%2F2txt.vercel.app%2Fopengraph-image.png">
-							deploy
-						</A>
+						<A href="https://github.com/petroff69/Sentient-Image-Descripton-Collector-Prototype">source</A>
 					</p>
 				</footer>
 
